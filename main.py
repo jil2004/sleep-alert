71bcaf1ac25470bc045b64e54082782491082515
jil2004 <110085243+jil2004@users.noreply.github.com>
2023-04-19 23:38:05 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+import cv2
+from playsound import playsound
+import time
+
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+start_time = time.time()
+eyes_closed = False
+
+while True:
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 2)
+
+        if len(eyes) == 0:
+            if not eyes_closed:
+                start_time = time.time()
+                eyes_closed = True
+            else:
+                elapsed_time = time.time() - start_time
+                if elapsed_time > 5:
+                    playsound("alarm.mp3")
+        else:
+            eyes_closed = False
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
